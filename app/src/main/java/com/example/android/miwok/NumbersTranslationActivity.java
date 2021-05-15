0c480158e0327c0c91fb0090aa7b55e0484bccdf
PeterCai7 <caiju33@sina.com>
2021-05-15 18:17:43 -0400

    Copy from Udacity and create my own repository

---
+++ app/src/main/java/com/example/android/miwok/NumbersTranslationActivity.java
@@ -0,0 +1,44 @@
+package com.example.android.miwok;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class NumbersTranslationActivity extends AppCompatActivity {
+    private final static String LOG_TAG = "NumbersTransActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numbers_translation);
+
+        String[] wordsArray = {"one", "two", "three", "four", "five",
+                "six", "seven", "eight", "nine", "ten"};
+        ArrayList<String> words = new ArrayList<>(Arrays.asList(wordsArray));
+
+        //LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        //using a while loop to add wordViews repetitively
+        //discard this method due to limited memory
+        /*Iterator<String> iterator = words.iterator();
+        while(iterator.hasNext()) {
+            TextView wordView = new TextView(this);
+            wordView.setText(iterator.next());
+            rootView.addView(wordView);
+        }*/
+        ArrayAdapter<String> itemsAdaptor = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdaptor);
+
+    }
+}
