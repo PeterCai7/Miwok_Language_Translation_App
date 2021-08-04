2a8893bf78d696f7bdd283faf2dbb68a39cbafc4
Peter <petercai1995@gmail.com>
2021-08-04 14:30:06 -0400

    Add in audios

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -15,6 +15,7 @@ public class Word {
     //image resource ID
     private int imgResId = NO_IMAGE_PROVIDED;
 
+    private int mAudioResourceId;
 
     /**
      * Constructor method of Word class for Phrases
@@ -22,9 +23,10 @@ public class Word {
      * @param defaultTranslation
      * @param miwokTranslation
      */
-    public Word(String defaultTranslation, String miwokTranslation) {
+    public Word(String defaultTranslation, String miwokTranslation, int mAudioResourceId) {
         this.defaultTranslation = defaultTranslation;
         this.miwokTranslation = miwokTranslation;
+        this.mAudioResourceId = mAudioResourceId;
     }
 
     /**
@@ -33,11 +35,13 @@ public class Word {
      * @param defaultTranslation
      * @param miwokTranslation
      * @param imgResId
+     * @param mAudioResourceId
      */
-    public Word(String defaultTranslation, String miwokTranslation, int imgResId) {
+    public Word(String defaultTranslation, String miwokTranslation, int imgResId, int mAudioResourceId) {
         this.defaultTranslation = defaultTranslation;
         this.miwokTranslation = miwokTranslation;
         this.imgResId = imgResId;
+        this.mAudioResourceId = mAudioResourceId;
     }
 
     //Getter methods
@@ -48,6 +52,7 @@ public class Word {
         return defaultTranslation;
     }
     public int getImgResId() { return imgResId; }
+    public int getAudioResourceId() {return mAudioResourceId;}
 
     /**
      * Returns whether or not there is an image for this word
