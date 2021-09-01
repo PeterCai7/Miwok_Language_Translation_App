489c53a6b11f6d437980179bd4d906c92fd8c50b
Peter <petercai1995@gmail.com>
2021-09-02 06:57:00 +0800

    Finish numberFragment

--- app/src/main/java/com/example/android/miwok/activities/NumbersTranslationActivity.java
+++ app/src/main/java/com/example/android/miwok/activities/NumbersTranslationActivity.java
@@ -1,32 +1,19 @@
  package com.example.android.miwok.activities;
 
-import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-
 import com.example.android.miwok.R;
-import com.example.android.miwok.Word;
-import com.example.android.miwok.WordAdapter;
-
-import java.util.ArrayList;
+import com.example.android.miwok.fragments.NumbersFragment;
 
  public class NumbersTranslationActivity extends AppCompatActivity {
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.word_list);
-
-
-    }
-    @Override
-    protected void onStop() {
-        super.onStop();
-        releaseMediaPlayer();
-    }
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.activity_category);
+         getSupportFragmentManager().beginTransaction()
+                 .replace(R.id.container, new NumbersFragment())
+                 .commit();
+     }
 
 }
