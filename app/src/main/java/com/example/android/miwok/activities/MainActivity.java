b016786956980061a43f3ef906a2bca5f527fec7
Peter <petercai1995@gmail.com>
2021-09-02 19:19:17 +0800

    Add in tabs

--- app/src/main/java/com/example/android/miwok/activities/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/activities/MainActivity.java
@@ -15,12 +15,10 @@
  */
 package com.example.android.miwok.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
 
 import com.example.android.miwok.MiwokFragmentPagerAdapter;
 import com.example.android.miwok.R;
@@ -41,5 +39,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the adapter onto the view pager
         viewPager.setAdapter(adapter);
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
